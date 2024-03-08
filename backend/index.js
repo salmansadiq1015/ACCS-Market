3b6216760aa46d7e0992bcf2eabfcb9b5b87d987
Salman Sadiq <salmansadiq1015@gmail.com>
2024-03-08 14:25:28 +0500

    Mern Stack

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "../server/config/db.js";
+
+// Dotenv Config
+dotenv.config();
+
+// Database Call
+connectDB();
+
+// Middlewares
+const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+
+// Rest API Call
+app.use("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+// PORT
+const PORT = process.env.PORT || 5000;
+
+// Listening
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`.bgGreen.white.bold);
+});
