c9403666f48235f681c341aaf8b1afcc437ca9a9
Salman Sadiq <118099469+salmansadiq1015@users.noreply.github.com>
2024-03-11 01:09:36 +0500

    Update ordersRoutes.js

--- backend/routes/ordersRoutes.js
+++ backend/routes/ordersRoutes.js
@@ -6,10 +6,5 @@ const router = express.Router();
 // Buy Channel
 router.post("/channel/payment", createOrder);
 
-// Get All Payments
-// router.get("/all/payments");
-
-// Get User Payment
-// router.get("/user/channel/payments");
 
 export default router;
