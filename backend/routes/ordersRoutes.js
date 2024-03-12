5db9ab94d63de96ff8a35dc5df84fa6a24a98e0a
Salman Sadiq <118099469+salmansadiq1015@users.noreply.github.com>
2024-03-12 15:10:46 +0500

    Update ordersRoutes.js

--- backend/routes/ordersRoutes.js
+++ backend/routes/ordersRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-// import { createOrder } from "../controller/ordersController.js";
+import { createOrder } from "../controller/ordersController.js";
 
 const router = express.Router();
 
 // Buy Channel
-// router.post("/channel/payment", createOrder);
+router.post("/channel/payment", createOrder);
 
 
 export default router;
